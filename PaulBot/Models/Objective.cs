d021107ffeccab685a73ae912bae8ec58356fba0
Mikhail Mryasov <damaghack@gmail.com>
2024-06-15 00:32:04 +0700

    Rename folder, add properties to Objective.cs, add methods to ObjectiveProcessor.cs

--- PaulBot/Models/Objective.cs
+++ PaulBot/Models/Objective.cs
@@ -9,18 +9,22 @@ enum Status
 
 class Objective
 {
-  public Objective(int _id, string _objectiveInfo, TimeSpan _expire)
+  public Objective(int _id, string _objectiveInfo, string _objectiveCreatorTag, TimeSpan _expire)
   {
     Id = _id;
     ObjectiveInfo = _objectiveInfo;
+    ObjectiveCreatorTag = _objectiveCreatorTag;
   }
 
   public void SetStatus(Status _state) => Status = _state;
+  public void SetExecutor(string _executorTag) => ExecutorTag = _executorTag;
 
   public int Id { get; init; }
   public string ObjectiveInfo { get; init; }
   public Status Status { get; private set; } = Status.Assigning;
   public DateTime CreatedAt { get; init; } = DateTime.Now;
+  public string ExecutorTag { get; private set; }
+  public string ObjectiveCreatorTag { get; init; }
 }
 
 
