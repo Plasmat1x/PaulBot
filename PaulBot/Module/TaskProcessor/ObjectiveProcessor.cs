5c593486311bfb7b0ddd4d6b9fbb69cba7d15d63
Mikhail Mryasov <damaghack@gmail.com>
2024-06-14 23:17:08 +0700

    Update ObjectiveProcessor.cs

--- PaulBot/Module/TaskProcessor/ObjectiveProcessor.cs
+++ PaulBot/Module/TaskProcessor/ObjectiveProcessor.cs
@@ -14,5 +14,5 @@ class ObjectiveProcessor
 
   public Objective CreateTask(int _id, string _taskInfo, TimeSpan _expire) => new Objective(_id, _taskInfo, _expire);
   public Status TaskStatus(int _id) => throw new NotImplementedException();
-  public void UpdateStatus(Status _status) => throw new NotImplementedException();
+  public void UpdateStatus(int _id, Status _status) => throw new NotImplementedException();
 }
