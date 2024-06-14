7c2b283277773f6213ac7e6dd7b0810b03055090
Mikhail Mryasov <damaghack@gmail.com>
2024-06-14 21:49:24 +0700

    Rename Task to Objective

---
+++ PaulBot/Models/Objective.cs
@@ -0,0 +1,27 @@
+﻿namespace PaulBot.Models;
+
+enum Status
+{
+  Completed,
+  Assigning,
+  Processing,
+}
+
+class Objective
+{
+  public Objective(int _id, string _objectiveInfo, TimeSpan _expire)
+  {
+    Id = _id;
+    ObjectiveInfo = _objectiveInfo;
+  }
+
+  public void SetStatus(Status _state) => Status = _state;
+
+  public int Id { get; init; }
+  public string ObjectiveInfo { get; init; }
+  public Status Status { get; private set; } = Status.Assigning;
+  public DateTime CreatedAt { get; init; } = DateTime.Now;
+}
+
+
+
---
+++ PaulBot/Module/TaskProcessor/ObjectiveProcessor.cs
@@ -0,0 +1,18 @@
+﻿using PaulBot.Models;
+
+namespace PaulBot.Module.ObjectiveProcessor;
+
+class ObjectiveProcessor
+{
+  //TODO: IStorage
+
+  //TODO: Add ILogger
+  public ObjectiveProcessor()
+  {
+
+  }
+
+  public Objective CreateTask(int _id, string _taskInfo, TimeSpan _expire) => new Objective(_id, _taskInfo, _expire);
+  public Status TaskStatus(int _id) => throw new NotImplementedException();
+  public void UpdateStatus(Status _status) => throw new NotImplementedException();
+}
