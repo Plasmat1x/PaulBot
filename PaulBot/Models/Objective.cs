ba20a9e38894073ec7ebc08b107deee9d3ec2fd3
Mikhail Mryasov <27920897+Plasmat1x@users.noreply.github.com>
2024-06-15 16:45:45 +0700

    Feature/objective processord (#5)
    * Adjusting folder name
    
    * Add test for ObjectiveProceessor, adusting namings om ObjectiveProcessor
    
    * Add expire property to objective, adjusting test

--- PaulBot/Models/Objective.cs
+++ PaulBot/Models/Objective.cs
@@ -1,19 +1,20 @@
 ﻿namespace PaulBot.Models;
 
-enum Status
+public enum Status
 {
   Completed,
   Assigning,
   Processing,
 }
 
-class Objective
+public class Objective
 {
   public Objective(int _id, string _objectiveInfo, string _objectiveCreatorTag, TimeSpan _expire)
   {
     Id = _id;
     ObjectiveInfo = _objectiveInfo;
     ObjectiveCreatorTag = _objectiveCreatorTag;
+    Expire = CreatedAt.Add(_expire);
   }
 
   public void SetStatus(Status _state) => Status = _state;
@@ -21,6 +22,7 @@ class Objective
 
   public int Id { get; init; }
   public string ObjectiveInfo { get; init; }
+  public DateTime Expire { get; init; }
   public Status Status { get; private set; } = Status.Assigning;
   public DateTime CreatedAt { get; init; } = DateTime.Now;
   public string ExecutorTag { get; private set; }
---
+++ PaulBot_Test/Modules/ObjectiveProcessorTest.cs
@@ -0,0 +1,29 @@
+﻿using PaulBot.Models;
+using PaulBot.Module.ObjectiveProcessor;
+
+namespace PaulBot_Test.Modules;
+public class ObjectiveProcessorTest
+{
+
+  [Fact]
+  public void create_objective_With_objectiveprocessor()
+  {
+    int id = 1;
+    string objectiveInfo = "Test creation";
+    string objectiveCreatorTag = "DiscordUser";
+    TimeSpan expirie = TimeSpan.FromMilliseconds(10);
+
+    var objectiveProcessor = new ObjectiveProcessor();
+
+    var exceptedObjective = new Objective(id, objectiveInfo, objectiveCreatorTag, expirie);
+
+    var creationObjectiveResult = objectiveProcessor.CreateObjective(new(id, objectiveInfo, objectiveCreatorTag, expirie));
+
+    bool assertResult = (creationObjectiveResult.Status == exceptedObjective.Status)
+      && (creationObjectiveResult.Id == exceptedObjective.Id)
+      && (creationObjectiveResult.ObjectiveInfo == exceptedObjective.ObjectiveInfo)
+      && (creationObjectiveResult.ExecutorTag == exceptedObjective.ExecutorTag);
+
+    Assert.True(assertResult);
+  }
+}
