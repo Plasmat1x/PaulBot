39ab29764100dfd61fea66c4df7b3b6694aa68cf
Mikhail Mryasov <27920897+Plasmat1x@users.noreply.github.com>
2024-06-16 16:51:45 +0700

    Features/polling system (#6)
    * Add PollingSystem interface and implementor
    
    * Add Interfaces: ILogger, IStorage
    Add classes CommandParser
    
    * Adjust ObjectiveProcessor, IStorage, Objective
    Add Test to Objective, Adjust test for ObjectiveProcessor
    
    * Update ObjectiveProcessord

---
+++ PaulBot/Modules/Interfaces/IStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace PaulBot.Modules.Interfaces;
+public interface IStorage<T>
+{
+  Task<T> ReadAsync(int _id, CancellationToken _cancelationToken = default);
+  Task<T> CreateAsync(T _entity, CancellationToken _cancelationToken = default);
+  Task<T> UpdateAsync(T _entity, CancellationToken _cancelationToken = default);
+  Task<T> DeleteAsync(T _entity, CancellationToken _cancelationToken = default);
+}
---
+++ PaulBot_Test/Models/ObjectiveTest.cs
@@ -0,0 +1,54 @@
+﻿using PaulBot.Models;
+
+namespace PaulBot_Test.Models;
+public class ObjectiveTest
+{
+  [Fact]
+  public void create_objective_Should_initilize_properties()
+  {
+    // Arrange
+    int id = 1;
+    string info = "Test Objective";
+    string creatorTag = "Creator123";
+    DateTime expire = DateTime.UtcNow.AddHours(1);
+
+    // Act
+    var objective = new Objective(id, info, creatorTag, expire);
+
+    // Assert
+    Assert.Equal(id, objective.Id);
+    Assert.Equal(info, objective.ObjectiveInfo);
+    Assert.Equal(creatorTag, objective.ObjectiveCreatorTag);
+    Assert.Equal(expire, objective.Expire);
+    Assert.Equal(Status.Assigning, objective.Status);
+    Assert.Equal(DateTime.UtcNow.Hour, objective.CreatedAt.Hour);
+  }
+
+  [Fact]
+  public void with_status_Should_return_new_objective_With_updated_status()
+  {
+    // Arrange
+    var objective = new Objective(1, "Test", "Creator123", DateTime.UtcNow.AddHours(1));
+
+    // Act
+    var updatedObjective = objective.SetStatus(Status.Completed);
+
+    // Assert
+    Assert.NotSame(objective, updatedObjective);
+    Assert.Equal(Status.Completed, updatedObjective.Status);
+  }
+
+  [Fact]
+  public void WithExecutor_ShouldReturnNewObjectiveWithUpdatedExecutor()
+  {
+    // Arrange
+    var objective = new Objective(1, "Test", "Creator123", DateTime.UtcNow.AddHours(1));
+
+    // Act
+    var updatedObjective = objective.SetExecutor("Executor456");
+
+    // Assert
+    Assert.NotSame(objective, updatedObjective);
+    Assert.Equal("Executor456", updatedObjective.ExecutorTag);
+  }
+}
