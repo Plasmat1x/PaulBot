9ad295b056cba61f572e5543e5e573b0cd6534ef
yksyc147 <164842897+yksyc147@users.noreply.github.com>
2024-06-17 12:08:35 +0500

    Update ChannelPolling.cs

--- PaulBot/Modules/PollingSystem/ChannelPolling.cs
+++ PaulBot/Modules/PollingSystem/ChannelPolling.cs
@@ -1,13 +1,15 @@
 ﻿using PaulBot.Modules.Interfaces;
 
 namespace PaulBot.Modules.PollingSystem;
+// Реализация интерфейса IPollingSystem
 public class ChannelPolling: IPollingSystem
 {
+  // Конструктор по умолчанию
   public ChannelPolling()
   {
 
   }
-
+  // Метод Polling, отвечающий за опросы
   public void Polling()
   {
     throw new NotImplementedException();
