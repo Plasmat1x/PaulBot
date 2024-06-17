49eb53a4239d49d9eb3abbc7a6ccd1f6e7bd32e3
Rinat Idrisov <idrisoff1986@mail.ru>
2024-06-17 18:11:07 +0500

    EfCoreStorage

---
+++ PaulBot/Storage/EfCoreStorage.cs
@@ -0,0 +1,48 @@
+﻿using PaulBot.Models;
+using PaulBot.Modules.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulBot.Storage
+{
+  public class EfCoreStorage<T> : IStorage<T> where T : class
+  {
+    private readonly DbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+
+    public EfCoreStorage(DbContext dbContext)
+    {
+      _dbContext = dbContext;
+      _dbSet = _dbContext.Set<T>();
+    }
+
+    public async Task<T> ReadAsync(int id, CancellationToken cancellationToken = default)
+    {
+      return await _dbSet.FindAsync(new object[] { id }, cancellationToken);
+    }
+
+    public async Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+      _dbSet.Add(entity);
+      await _dbContext.SaveChangesAsync(cancellationToken);
+      return entity;
+    }
+
+    public async Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default)
+    {
+      _dbContext.Entry(entity).State = EntityState.Modified;
+      await _dbContext.SaveChangesAsync(cancellationToken);
+      return entity;
+    }
+
+    public async Task<T> DeleteAsync(T entity, CancellationToken cancellationToken = default)
+    {
+      _dbSet.Remove(entity);
+      await _dbContext.SaveChangesAsync(cancellationToken);
+      return entity;
+    }
+  }
+}
