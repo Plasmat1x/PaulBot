adaa9ad08e95152b477fb9ec06bfc99bc7445bdd
yksyc147 <164842897+yksyc147@users.noreply.github.com>
2024-06-17 10:13:29 +0500

    Update CommandParser.cs

--- PaulBot/Modules/CommandParser/CommandParser.cs
+++ PaulBot/Modules/CommandParser/CommandParser.cs
@@ -1,10 +1,12 @@
 ﻿namespace PaulBot.Modules.CommandParser;
+// Класс для парсинга команд
 public class CommandParser
 {
+  // Конструктор парсера
   public CommandParser()
   {
 
   }
-
+  // Метод для парсинга строки ввода
   public string Parse(string input) => throw new NotImplementedException();
 }
